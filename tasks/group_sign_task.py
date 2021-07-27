515b8c3808fd08ec3edd79e7dc5ce16c7f7f1a0c
MaxSecurity <2271150345@qq.com>
2021-07-27 18:24:26 +0800

    多余参数报错修改。

--- tasks/group_sign_task.py
+++ tasks/group_sign_task.py
@@ -15,18 +15,21 @@ async def group_sign_task(biliapi: asyncbili) -> None:
         return
 
     su, er = 0, 0
-    for group in ret["data"]["list"]:
-        try:
-            sign = await biliapi.groupSign(group["group_id"], group["owner_uid"])
-        except Exception as e:
-            logging.warning(f'{biliapi.name}: 给{group["group_name"]}应援异常，原因为{str(e)}')
-            er += 1
-        else:
-            if sign["code"] == 0 and sign["data"]["status"] == 0:
-                logging.info(f'{biliapi.name}: 给{group["group_name"]}应援成功')
-                su += 1
-            else:
-                logging.info(f'{biliapi.name}: 给{group["group_name"]}应援失败，原因为:{sign["message"]}')
+    try:
+        for group in ret["data"]["list"]:
+            try:
+                sign = await biliapi.groupSign(group["group_id"], group["owner_uid"])
+            except Exception as e:
+                logging.warning(f'{biliapi.name}: 给{group["group_name"]}应援异常，原因为{str(e)}')
                 er += 1
-    if er > 0:
-        webhook.addMsg('msg_simple', f'{biliapi.name}:应援团签到成功{su}个,失败{er}个\n')
+            else:
+                if sign["code"] == 0 and sign["data"]["status"] == 0:
+                    logging.info(f'{biliapi.name}: 给{group["group_name"]}应援成功')
+                    su += 1
+                else:
+                    logging.info(f'{biliapi.name}: 给{group["group_name"]}应援失败，原因为:{sign["message"]}')
+                    er += 1
+        if er > 0:
+            webhook.addMsg('msg_simple', f'{biliapi.name}:应援团签到成功{su}个,失败{er}个\n')
+    except Exception as e:
+        logging.warning(f'{biliapi.name}: 应援团字典里不存在的”key“产生的错误，原因为{str(e)}，跳过应援团签到')
